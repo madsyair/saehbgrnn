bbdb0dfd6a2b09219c1d2bb47a129c69ffc93b2f
madsyair <madsyair@bps.go.id>
2017-03-14 13:49:42 +0700

    correction

--- R/auto.saehbgrnn.R
+++ R/auto.saehbgrnn.R
@@ -1,8 +1,8 @@
 auto.saehbgrnn<-function(y=NULL,n=NULL,xl=NULL,xnl=NULL,M=5,adapt=4000,startburnin=1000,nChains = 2,startsample=10000,thinSteps=20,DIC=FALSE,scale=TRUE)
 {
   if(scale) {
-    xl <- scale(xl)
-    xnl <- scale(xnl)
+    xl <- as.matrix(scale(xl))
+    xnl <- as.matrix(scale(xnl))
   }
   pkgs <- c('ggmcmc')
   lapply(pkgs, require, character.only = T)
--- R/sae.hbgrnn.R
+++ R/sae.hbgrnn.R
@@ -1,8 +1,8 @@
 sae.hbgrnn<-function(y=NULL,n=NULL,xl=NULL,xnl=NULL,M=5,adapt=4000,burnin=1000,nChains = 2,sample=10000,thin=20,DIC=FALSE,scale=TRUE)
 {
   if(scale) {
-    xl <- scale(xl)
-    xnl <- scale(xnl)
+    xl <- as.matrix(scale(xl))
+    xnl <- as.matrix(scale(xnl))
      }
   pkgs <- c('ggmcmc')
   lapply(pkgs, require, character.only = T)
