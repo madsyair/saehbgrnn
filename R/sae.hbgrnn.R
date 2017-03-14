9d3c2c7059d252ef84d3d4f46335e1b8fcb484ac
madsyair <madsyair@bps.go.id>
2017-03-14 11:33:15 +0700

    saehbgrnn using runjags

--- R/sae.hbgrnn.R
+++ R/sae.hbgrnn.R
@@ -1,5 +1,8 @@
-sae.hbgrnn<-function(y=NULL,n=NULL,xl=NULL,xnl=NULL,M=5,adapt=4000,burnin=1000,nChains = 2,sample=10000,thin=20,DIC=FALSE)
+sae.hbgrnn<-function(y=NULL,n=NULL,xl=NULL,xnl=NULL,M=5,adapt=4000,burnin=1000,nChains = 2,sample=10000,thin=20,DIC=FALSE,scale=TRUE)
 {
+  if(scale) {
+    x <- scale(x)
+     }
   pkgs <- c('ggmcmc')
   lapply(pkgs, require, character.only = T)
   nulxl=is.null(xl)
