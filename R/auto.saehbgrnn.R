d3abb424348ea202d093716979bb2efc38231ba2
madsyair <madsyair@bps.go.id>
2017-03-16 07:53:27 +0700

    saehbgrnn using autorun in runjags

--- R/auto.saehbgrnn.R
+++ R/auto.saehbgrnn.R
@@ -397,7 +397,8 @@ auto.saehbgrnn<-function(y=NULL,n=NULL,xl=NULL,xnl=NULL,M=5,adapt=4000,startburn
   ggso<-ggs(coda)
   t<-Sys.time()
   nt <- as.POSIXlt(t)
-  ggmcmc(ggso,file=paste("auto",type,date(t),"-",nt[[3]],"-",nt[[2]],"-",floor(nt[[1]]),".pdf",sep=""))
+  dt<-date(t)
+  ggmcmc(ggso,file=paste("auto",type,dt,"-",nt[[3]],"-",nt[[2]],"-",floor(nt[[1]]),".pdf",sep=""))
   
   mcmc = as.matrix(coda,chains=TRUE)
   summaryout<-summary(obj)
