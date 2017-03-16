b6d43b10dd8b6b8c40c2f8622615dabed20e587a
madsyair <madsyair@bps.go.id>
2017-03-17 05:34:19 +0700

    saehbgrnn using runjags

--- R/sae.hbgrnn.R
+++ R/sae.hbgrnn.R
@@ -1,4 +1,4 @@
-saehbgrnn<-function(y=NULL,n=NULL,xl=NULL,xnl=NULL,M=5,adapt=4000,burnin=1000,nChains = 2,sample=10000,thin=20,DIC=FALSE,scale=TRUE)
+saehbgrnn<-function(y=NULL,n=NULL,xl=NULL,xnl=NULL,M=5,adapt=4000,burnin=1000,nChains = 2,sample=10000,thin=1,DIC=FALSE,scale=TRUE)
 {
    
   pkgs <- c('rjags',  'runjags','coda','ggmcmc')
